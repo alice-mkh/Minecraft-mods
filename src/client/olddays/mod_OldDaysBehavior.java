b26ca5398b7bf01ae2f0eb45d94878398fe9ba57
Exalm <armageddon.96.96@gmail.com>
2012-03-30 16:36:31 +0600

    OldDays: Spiders

--- src/client/olddays/mod_OldDaysBehavior.java
+++ src/client/olddays/mod_OldDaysBehavior.java
@@ -8,8 +8,10 @@ public class mod_OldDaysBehavior extends mod_OldDays{
         addProperty(this, 1, "Old mob AI",             false, "OldAI");
         addProperty(this, 2, "Animal panic",           true,  "AnimalsFlee");
         addProperty(this, 3, "Sheep eat grass",        true,  "SheepEatGrass");
-        addProperty(this, 4, "Survival Test creepers", false, "SurvivalTestCreepers");
-        addProperty(this, 5, "Survival Test zombies",  false, "SurvivalTestZombies");
+        addProperty(this, 4, "Spiders climb walls",    true,  "SpidersCanClimb");
+        addProperty(this, 5, "Survival Test creepers", false, "SurvivalTestCreepers");
+        addProperty(this, 6, "Survival Test zombies",  false, "SurvivalTestZombies");
+        addProperty(this, 7, "Survival Test spiders",  false, "SurvivalTestSpiders");
         loadModuleProperties(4);
     }
 
@@ -22,15 +24,19 @@ public class mod_OldDaysBehavior extends mod_OldDays{
             case 2: EntityAIPanic.disablePanic =   !AnimalsFlee;
                     EntityCreature.nopanic =       !AnimalsFlee;          break;
             case 3: EntityAIEatGrass2.caneatgrass = SheepEatGrass;        break;
-            case 4: EntityCreeper.survivaltest =    SurvivalTestCreepers; break;
-            case 5: EntityCreature.fastzombies =    SurvivalTestZombies;
+            case 4: EntitySpider.canclimb =         SpidersCanClimb;      break;
+            case 5: EntityCreeper.survivaltest =    SurvivalTestCreepers; break;
+            case 6: EntityCreature.fastzombies =    SurvivalTestZombies;
                     EntityZombie.burns =           !SurvivalTestZombies;  break;
+            case 7: EntitySpider.survivaltest =     SurvivalTestSpiders;  break;
         }
     }
 
     public static boolean OldAI;
     public static boolean AnimalsFlee = true;
     public static boolean SheepEatGrass = true;
+    public static boolean SpidersCanClimb = true;
     public static boolean SurvivalTestCreepers;
     public static boolean SurvivalTestZombies;
+    public static boolean SurvivalTestSpiders;
 }
