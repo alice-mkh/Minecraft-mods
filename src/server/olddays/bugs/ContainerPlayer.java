357d894255b48ff2b1a69e8ec8b1b741e28f4687
Exalm <armageddon.96.96@gmail.com>
2012-06-27 20:15:52 +0600

    OldDays: Crafting slots bug

---
+++ src/server/olddays/bugs/ContainerPlayer.java
@@ -0,0 +1,156 @@
+package net.minecraft.src;
+
+import java.util.List;
+
+public class ContainerPlayer extends Container
+{
+    public static boolean dropCrafting = true;
+
+    /** The crafting matrix inventory. */
+    public InventoryCrafting craftMatrix;
+    public IInventory craftResult;
+
+    /** Determines if inventory manipulation should be handled. */
+    public boolean isLocalWorld;
+
+    public ContainerPlayer(InventoryPlayer par1InventoryPlayer)
+    {
+        this(par1InventoryPlayer, true);
+    }
+
+    public ContainerPlayer(InventoryPlayer par1InventoryPlayer, boolean par2)
+    {
+        craftMatrix = new InventoryCrafting(this, 2, 2);
+        craftResult = new InventoryCraftResult();
+        isLocalWorld = false;
+        isLocalWorld = par2;
+        addSlot(new SlotCrafting(par1InventoryPlayer.player, craftMatrix, craftResult, 0, 144, 36));
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int i1 = 0; i1 < 2; i1++)
+            {
+                addSlot(new Slot(craftMatrix, i1 + i * 2, 88 + i1 * 18, 26 + i * 18));
+            }
+        }
+
+        for (int j = 0; j < 4; j++)
+        {
+            int j1 = j;
+            addSlot(new SlotArmor(this, par1InventoryPlayer, par1InventoryPlayer.getSizeInventory() - 1 - j, 8, 8 + j * 18, j1));
+        }
+
+        for (int k = 0; k < 3; k++)
+        {
+            for (int k1 = 0; k1 < 9; k1++)
+            {
+                addSlot(new Slot(par1InventoryPlayer, k1 + (k + 1) * 9, 8 + k1 * 18, 84 + k * 18));
+            }
+        }
+
+        for (int l = 0; l < 9; l++)
+        {
+            addSlot(new Slot(par1InventoryPlayer, l, 8 + l * 18, 142));
+        }
+
+        onCraftMatrixChanged(craftMatrix);
+    }
+
+    /**
+     * Callback for when the crafting matrix is changed.
+     */
+    public void onCraftMatrixChanged(IInventory par1IInventory)
+    {
+        craftResult.setInventorySlotContents(0, CraftingManager.getInstance().findMatchingRecipe(craftMatrix));
+    }
+
+    /**
+     * Callback for when the crafting gui is closed.
+     */
+    public void onCraftGuiClosed(EntityPlayer par1EntityPlayer)
+    {
+        super.onCraftGuiClosed(par1EntityPlayer);
+
+        if (dropCrafting){
+            for (int i = 0; i < 4; i++)
+            {
+                ItemStack itemstack = craftMatrix.getStackInSlotOnClosing(i);
+
+                if (itemstack != null)
+                {
+                    par1EntityPlayer.dropPlayerItem(itemstack);
+                }
+            }
+        }
+
+        craftResult.setInventorySlotContents(0, null);
+    }
+
+    public boolean canInteractWith(EntityPlayer par1EntityPlayer)
+    {
+        return true;
+    }
+
+    /**
+     * Called to transfer a stack from one inventory to the other eg. when shift clicking.
+     */
+    public ItemStack transferStackInSlot(int par1)
+    {
+        ItemStack itemstack = null;
+        Slot slot = (Slot)inventorySlots.get(par1);
+
+        if (slot != null && slot.getHasStack())
+        {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+
+            if (par1 == 0)
+            {
+                if (!mergeItemStack(itemstack1, 9, 45, true))
+                {
+                    return null;
+                }
+
+                slot.func_48417_a(itemstack1, itemstack);
+            }
+            else if (par1 >= 9 && par1 < 36)
+            {
+                if (!mergeItemStack(itemstack1, 36, 45, false))
+                {
+                    return null;
+                }
+            }
+            else if (par1 >= 36 && par1 < 45)
+            {
+                if (!mergeItemStack(itemstack1, 9, 36, false))
+                {
+                    return null;
+                }
+            }
+            else if (!mergeItemStack(itemstack1, 9, 45, false))
+            {
+                return null;
+            }
+
+            if (itemstack1.stackSize == 0)
+            {
+                slot.putStack(null);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+
+            if (itemstack1.stackSize != itemstack.stackSize)
+            {
+                slot.onPickupFromSlot(itemstack1);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        return itemstack;
+    }
+}
--- src/server/olddays/mod_OldDaysBugs.java
+++ src/server/olddays/mod_OldDaysBugs.java
@@ -3,12 +3,13 @@ package net.minecraft.src;
 public class mod_OldDaysBugs extends mod_OldDays{
     public void load(){
         registerModule(this, 1);
-        addProperty(1, "Boosters",      false, true,  "Boosters",     "");
-        addProperty(2, "Water lifts",   false, true,  "WaterLifts",   "");
-        addProperty(3, "Toasters",      false, true,  "LavaToasters", "");
-        addProperty(4, "Water ladders", false, true,  "WaterLadders", "");
-        addProperty(5, "Ladder gaps",   false, true,  "LadderGaps",   "");
-        addProperty(6, "Piston dupe",   false, false, "PistonDupe",   "");
+        addProperty(1, "Boosters",       false, true,  "Boosters",      "");
+        addProperty(2, "Water lifts",    false, true,  "WaterLifts",    "");
+        addProperty(3, "Toasters",       false, true,  "LavaToasters",  "");
+        addProperty(4, "Water ladders",  false, true,  "WaterLadders",  "");
+        addProperty(5, "Ladder gaps",    false, true,  "LadderGaps",    "");
+        addProperty(6, "Piston dupe",    false, false, "PistonDupe",    "");
+        addProperty(7, "Crafting slots", false, false, "CraftingSlots", "");
         loadModuleProperties();
     }
 
@@ -20,6 +21,7 @@ public class mod_OldDaysBugs extends mod_OldDays{
             case 4: setBool(net.minecraft.src.Entity.class, "waterladder", WaterLadders); break;
             case 5: setBool(net.minecraft.src.EntityLiving.class, "laddergaps", LadderGaps); break;
             case 6: setBool(net.minecraft.src.BlockPistonBase.class, "dupe", PistonDupe); break;
+            case 7: setBool(net.minecraft.src.ContainerPlayer.class, "dropCrafting", !CraftingSlots); break;
         }
     }
 
@@ -29,6 +31,5 @@ public class mod_OldDaysBugs extends mod_OldDays{
     public static boolean LavaToasters = true;
     public static boolean WaterLifts = true;
     public static boolean PistonDupe;
-//Sand generator
-//Data value change bug
+    public static boolean CraftingSlots;
 }
