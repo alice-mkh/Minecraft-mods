2685148ee4a7ba8247bd5eb52e7eb4d774e0f194
Exalm <armageddon.96.96@gmail.com>
2012-07-10 22:24:13 +0600

    1.3: Fixed temple lever rotation

--- src/client/nbxlite/backport/ComponentScatteredFeature.java
+++ src/client/nbxlite/backport/ComponentScatteredFeature.java
@@ -65,4 +65,329 @@ abstract class ComponentScatteredFeature extends StructureComponent
             return true;
         }
     }
+
+    protected int getMetadataWithOffset(int par1, int par2)
+    {
+        if (par1 == Block.rail.blockID)
+        {
+            if (coordBaseMode == 1 || coordBaseMode == 3)
+            {
+                return par2 != 1 ? 1 : 0;
+            }
+        }
+        else if (par1 == Block.doorWood.blockID || par1 == Block.doorSteel.blockID)
+        {
+            if (coordBaseMode == 0)
+            {
+                if (par2 == 0)
+                {
+                    return 2;
+                }
+
+                if (par2 == 2)
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                if (coordBaseMode == 1)
+                {
+                    return par2 + 1 & 3;
+                }
+
+                if (coordBaseMode == 3)
+                {
+                    return par2 + 3 & 3;
+                }
+            }
+        }
+        else if (par1 == Block.stairCompactCobblestone.blockID || par1 == Block.stairCompactPlanks.blockID || par1 == Block.stairsNetherBrick.blockID || par1 == Block.stairsStoneBrickSmooth.blockID || par1 == 128)
+        {
+            if (coordBaseMode == 0)
+            {
+                if (par2 == 2)
+                {
+                    return 3;
+                }
+
+                if (par2 == 3)
+                {
+                    return 2;
+                }
+            }
+            else if (coordBaseMode == 1)
+            {
+                if (par2 == 0)
+                {
+                    return 2;
+                }
+
+                if (par2 == 1)
+                {
+                    return 3;
+                }
+
+                if (par2 == 2)
+                {
+                    return 0;
+                }
+
+                if (par2 == 3)
+                {
+                    return 1;
+                }
+            }
+            else if (coordBaseMode == 3)
+            {
+                if (par2 == 0)
+                {
+                    return 2;
+                }
+
+                if (par2 == 1)
+                {
+                    return 3;
+                }
+
+                if (par2 == 2)
+                {
+                    return 1;
+                }
+
+                if (par2 == 3)
+                {
+                    return 0;
+                }
+            }
+        }
+        else if (par1 == Block.ladder.blockID)
+        {
+            if (coordBaseMode == 0)
+            {
+                if (par2 == 2)
+                {
+                    return 3;
+                }
+
+                if (par2 == 3)
+                {
+                    return 2;
+                }
+            }
+            else if (coordBaseMode == 1)
+            {
+                if (par2 == 2)
+                {
+                    return 4;
+                }
+
+                if (par2 == 3)
+                {
+                    return 5;
+                }
+
+                if (par2 == 4)
+                {
+                    return 2;
+                }
+
+                if (par2 == 5)
+                {
+                    return 3;
+                }
+            }
+            else if (coordBaseMode == 3)
+            {
+                if (par2 == 2)
+                {
+                    return 5;
+                }
+
+                if (par2 == 3)
+                {
+                    return 4;
+                }
+
+                if (par2 == 4)
+                {
+                    return 2;
+                }
+
+                if (par2 == 5)
+                {
+                    return 3;
+                }
+            }
+        }
+        else if (par1 == Block.button.blockID)
+        {
+            if (coordBaseMode == 0)
+            {
+                if (par2 == 3)
+                {
+                    return 4;
+                }
+
+                if (par2 == 4)
+                {
+                    return 3;
+                }
+            }
+            else if (coordBaseMode == 1)
+            {
+                if (par2 == 3)
+                {
+                    return 1;
+                }
+
+                if (par2 == 4)
+                {
+                    return 2;
+                }
+
+                if (par2 == 2)
+                {
+                    return 3;
+                }
+
+                if (par2 == 1)
+                {
+                    return 4;
+                }
+            }
+            else if (coordBaseMode == 3)
+            {
+                if (par2 == 3)
+                {
+                    return 2;
+                }
+
+                if (par2 == 4)
+                {
+                    return 1;
+                }
+
+                if (par2 == 2)
+                {
+                    return 3;
+                }
+
+                if (par2 == 1)
+                {
+                    return 4;
+                }
+            }
+        }
+        else if (par1 == 131 || Block.blocksList[par1] != null && (Block.blocksList[par1] instanceof BlockDirectional))
+        {
+            if (coordBaseMode == 0)
+            {
+                if (par2 == 0 || par2 == 2)
+                {
+                    return Direction.footInvisibleFaceRemap[par2];
+                }
+            }
+            else if (coordBaseMode == 1)
+            {
+                if (par2 == 2)
+                {
+                    return 1;
+                }
+
+                if (par2 == 0)
+                {
+                    return 3;
+                }
+
+                if (par2 == 1)
+                {
+                    return 2;
+                }
+
+                if (par2 == 3)
+                {
+                    return 0;
+                }
+            }
+            else if (coordBaseMode == 3)
+            {
+                if (par2 == 2)
+                {
+                    return 3;
+                }
+
+                if (par2 == 0)
+                {
+                    return 1;
+                }
+
+                if (par2 == 1)
+                {
+                    return 2;
+                }
+
+                if (par2 == 3)
+                {
+                    return 0;
+                }
+            }
+        }
+        else if (par1 == Block.pistonBase.blockID || par1 == Block.pistonStickyBase.blockID || par1 == Block.lever.blockID || par1 == Block.dispenser.blockID)
+        {
+            if (coordBaseMode == 0)
+            {
+                if (par2 == 2 || par2 == 3)
+                {
+                    return Facing.faceToSide[par2];
+                }
+            }
+            else if (coordBaseMode == 1)
+            {
+                if (par2 == 2)
+                {
+                    return 4;
+                }
+
+                if (par2 == 3)
+                {
+                    return 5;
+                }
+
+                if (par2 == 4)
+                {
+                    return 2;
+                }
+
+                if (par2 == 5)
+                {
+                    return 3;
+                }
+            }
+            else if (coordBaseMode == 3)
+            {
+                if (par2 == 2)
+                {
+                    return 5;
+                }
+
+                if (par2 == 3)
+                {
+                    return 4;
+                }
+
+                if (par2 == 4)
+                {
+                    return 2;
+                }
+
+                if (par2 == 5)
+                {
+                    return 3;
+                }
+            }
+        }
+
+        return par2;
+    }
 }
