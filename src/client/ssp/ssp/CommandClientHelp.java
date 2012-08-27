501f2cf20e3253cad34528eee837488c22270870
Exalm <armageddon.96.96@gmail.com>
2012-08-27 18:42:19 +0600

    Fixed help command for SSP

--- src/client/ssp/ssp/CommandClientHelp.java
+++ src/client/ssp/ssp/CommandClientHelp.java
@@ -1,6 +1,7 @@
 package net.minecraft.src;
 
 import java.util.List;
+import java.util.Map;
 import net.minecraft.client.Minecraft;
 
 public class CommandClientHelp extends CommandHelp
@@ -12,5 +13,10 @@ public class CommandClientHelp extends CommandHelp
     protected List func_71534_d(ICommandSender par1ICommandSender)
     {
         return Minecraft.getMinecraft().getCommandManager().getPossibleCommands(par1ICommandSender);
+    }
+
+    protected Map func_71535_c()
+    {
+        return Minecraft.getMinecraft().getCommandManager().getCommands();
     }
 }
