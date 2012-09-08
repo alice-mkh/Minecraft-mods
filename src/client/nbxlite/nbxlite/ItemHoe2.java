58edb64b52585d3091edb5c5db4d755c7cddf61a
Exalm <exalm7659@gmail.com>
2012-09-08 14:21:49 +0600

    Fixed hoes

--- src/client/nbxlite/nbxlite/ItemHoe2.java
+++ src/client/nbxlite/nbxlite/ItemHoe2.java
@@ -16,7 +16,7 @@ public class ItemHoe2 extends ItemHoe
      * Callback for item usage. If the item does something special on right clicking, he will have one of those. Return
      * True if something happen and false if it don't. This is for ITEMS, not BLOCKS !
      */
-    public boolean func_77648_a(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
+    public boolean tryPlaceIntoWorld(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, World par3World, int par4, int par5, int par6, int par7, float par8, float par9, float par10)
     {
         if (!par2EntityPlayer.canPlayerEdit(par4, par5, par6))
         {
