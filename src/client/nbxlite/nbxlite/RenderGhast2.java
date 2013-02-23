a84de1565a271f3891d502d897bd2eec3b02ce4c
Exalm <exalm7659@gmail.com>
2013-02-23 12:45:30 +0600

    Refactored ghast renderer a bit

--- src/client/nbxlite/nbxlite/RenderGhast2.java
+++ src/client/nbxlite/nbxlite/RenderGhast2.java
@@ -4,21 +4,15 @@ import org.lwjgl.opengl.GL11;
 import net.minecraft.client.Minecraft;
 import net.minecraft.src.ODNBXlite;
 import net.minecraft.src.EntityGhast;
-import net.minecraft.src.EntityLiving;
 import net.minecraft.src.ModelGhast;
 import net.minecraft.src.OpenGlHelper;
-import net.minecraft.src.RenderLiving;
+import net.minecraft.src.RenderGhast;
 
-public class RenderGhast2 extends RenderLiving
+public class RenderGhast2 extends RenderGhast
 {
     public static boolean bright = false;
 
-    public RenderGhast2()
-    {
-        super(new ModelGhast(), 0.5F);
-    }
-
-    protected void func_4014_a(EntityGhast entityghast, float f)
+    protected void preRenderGhast(EntityGhast entityghast, float f)
     {
         EntityGhast entityghast1 = entityghast;
         float f1 = ((float)entityghast1.prevAttackCounter + (float)(entityghast1.attackCounter - entityghast1.prevAttackCounter) * f) / 20F;
@@ -32,17 +26,12 @@ public class RenderGhast2 extends RenderLiving
         GL11.glScalef(f3, f2, f3);
         if (Minecraft.oldlighting){
             float ff = bright ? 1.0F : entityghast.getBrightness(f);
-            GL11.glColor3f(ff, ff, ff);
+            GL11.glColor4f(ff, ff, ff, 1.0F);
         }else{
             if (bright){
                 OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)(61680 % 0x10000) / 1.0F, (float)(61680 / 0x10000) / 1.0F);
             }
             GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         }
-    }
-
-    protected void preRenderCallback(EntityLiving entityliving, float f)
-    {
-        func_4014_a((EntityGhast)entityliving, f);
     }
 }
