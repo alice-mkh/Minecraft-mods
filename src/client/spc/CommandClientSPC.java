91bed58b1a4e3ed6d822d1aa07fcbdf3c2f9f2ce
Exalm <exalm7659@gmail.com>
2013-04-03 02:27:11 +0600

    Added missing @Overrides to SSPC

--- src/client/spc/CommandClientSPC.java
+++ src/client/spc/CommandClientSPC.java
@@ -13,20 +13,24 @@ public class CommandClientSPC extends CommandBase{
         this.desc = desc;
     }
 
+    @Override
     public String getCommandName(){
         return name;
     }
 /*
+    @Override
     public List getCommandAliases(){
         List list = new ArrayList();
         list.add("@" + name);
         return list;
     }
 */
+    @Override
     public String getCommandUsage(ICommandSender par1ICommandSender){
         return "/" + name + " " + desc;
     }
 
+    @Override
     public void processCommand(ICommandSender par1ICommandSender, String par2ArrayOfStr[]){
         String s = "/" + name;
         for (String str : par2ArrayOfStr){
@@ -39,6 +43,7 @@ public class CommandClientSPC extends CommandBase{
     /**
      * Returns true if the given command sender is allowed to use this command.
      */
+    @Override
     public boolean canCommandSenderUseCommand(ICommandSender par1ICommandSender){
         if (par1ICommandSender instanceof EntityPlayerSPSPC && !((EntityPlayerSPSPC)par1ICommandSender).canRunSPC()){
             return false;
--- src/client/spc/mod_SSPC.java
+++ src/client/spc/mod_SSPC.java
@@ -4,23 +4,28 @@ import net.minecraft.client.Minecraft;
 import java.util.ArrayList;
 
 public class mod_SSPC extends Mod{
+    @Override
     public void load(){
         //Replacing EntityPlayerSP2 class
         Minecraft.getMinecraft().playerClass = net.minecraft.src.EntityPlayerSPSPC.class;
     }
 
+    @Override
     public String getModName(){
         return "SSPC";
     }
 
+    @Override
     public String getModVersion(){
         return "3.3";
     }
 
+    @Override
     public String getMcVersion(){
         return EntityPlayerSPSPC.MCVERSION;
     }
 
+    @Override
     public void addSPCommands(ClientCommandManager manager){
         PlayerHelper.registerCommandWrappers(manager);
     }
