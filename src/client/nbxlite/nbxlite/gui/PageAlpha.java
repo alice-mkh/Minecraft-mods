048b1699d3df77736f2d07014386470633f3c88e
Exalm <exalm7659@gmail.com>
2013-04-30 19:56:30 +0600

    Snow: OFF/Random/ON button for Alpha page

--- src/client/nbxlite/nbxlite/gui/PageAlpha.java
+++ src/client/nbxlite/nbxlite/gui/PageAlpha.java
@@ -7,15 +7,18 @@ public class PageAlpha extends Page{
     private GuiButton[] featuresButtons;
     private GuiButton themeButton;
     private GuiButton newOresButton;
+    private GuiButton snowButton;
     private boolean newores;
     private int theme;
     private int features;
+    private int snow;
 
     public PageAlpha(GuiNBXlite parent){
         super(parent);
         featuresButtons = new GuiButton[ODNBXlite.BIOMELESS_FEATURES.length];
         newores = ODNBXlite.GenerateNewOres;
         theme = 0;
+        snow = 1;
         features = ODNBXlite.DefaultFeaturesBiomeless;
     }
 
@@ -31,40 +34,45 @@ public class PageAlpha extends Page{
         }
         buttonList.add(themeButton = new GuiButton(l, width / 2 - 85 + leftmargin, 0, 150, 20, ""));
         buttonList.add(newOresButton = new GuiButton(l + 1, width / 2 - 85 + leftmargin, 0, 150, 20, ""));
+        buttonList.add(snowButton = new GuiButton(l + 2, width / 2 - 85 + leftmargin, 0, 150, 20, ""));
         featuresButtons[features].enabled=false;
     }
 
     @Override
     public void scrolled(){
         for (int i = 0; i < featuresButtons.length; i++){
-            featuresButtons[i].yPosition = height / 6 + (i * 21) + scrolling;
+            featuresButtons[i].yPosition = height / 6 + ((i - 1) * 21) + scrolling;
         }
-        themeButton.yPosition = height / 6 + 85 + scrolling;
-        newOresButton.yPosition = height / 6 + 120 + scrolling;
+        themeButton.yPosition = height / 6 + 64 + scrolling;
+        newOresButton.yPosition = height / 6 + 99 + scrolling;
+        snowButton.yPosition = height / 6 + 121 + scrolling;
         updateButtonPosition();
     }
 
     @Override
     public int getContentHeight(){
-        return newOresButton.drawButton ? 120 : 108;
+        return snowButton.drawButton ? 121 : (newOresButton.drawButton ? 99 : 87);
     }
 
     @Override
     public void updateButtonText(){
         StringTranslate stringtranslate = StringTranslate.getInstance();
-        newOresButton.displayString = mod_OldDays.lang.get("nbxlite.generatenewores.name") + ": " + stringtranslate.translateKey("options." + (newores ? "on" : "off"));
+        String[] values = new String[]{"gui.off", "gui.random", "gui.on"};
+        newOresButton.displayString = mod_OldDays.lang.get("nbxlite.generatenewores.name") + ": " + mod_OldDays.lang.get(values[newores ? 2 : 0]);
         themeButton.displayString = mod_OldDays.lang.get("nbxlite.maptheme.name") + ": " + mod_OldDays.lang.get("nbxlite.maptheme" + (theme + 1));
+        snowButton.displayString = stringtranslate.translateKey("tile.snow.name") + ": " + mod_OldDays.lang.get(values[snow]);
     }
 
     @Override
     public void updateButtonVisibility(){
         newOresButton.drawButton = features > 0;
+        snowButton.drawButton = canSnow();
     }
 
     @Override
     public void drawScreen(int i, int j, float f){
         super.drawScreen(i, j, f);
-        drawCenteredString(fontRenderer, mod_OldDays.lang.get("nbxlite.maptheme" + (theme + 1) + ".desc"), width / 2 + leftmargin, height / 6 + 108 + scrolling, 0xa0a0a0);
+        drawCenteredString(fontRenderer, mod_OldDays.lang.get("nbxlite.maptheme" + (theme + 1) + ".desc"), width / 2 + leftmargin, height / 6 + 87 + scrolling, 0xa0a0a0);
     }
 
     @Override
@@ -80,6 +88,12 @@ public class PageAlpha extends Page{
                 theme++;
             }else{
                 theme = 0;
+            }
+        }else if (guibutton == snowButton){
+            if (snow < 2){
+                snow++;
+            }else{
+                snow = 0;
             }
         }else if (guibutton.id < featuresButtons.length){
             featuresButtons[features].enabled = true;
@@ -97,8 +111,14 @@ public class PageAlpha extends Page{
         ODNBXlite.Generator = ODNBXlite.GEN_BIOMELESS;
         ODNBXlite.MapFeatures = ODNBXlite.BIOMELESS_FEATURES[features];
         ODNBXlite.MapTheme = theme;
-        if(features == 3 && (ODNBXlite.MapTheme == ODNBXlite.THEME_NORMAL || ODNBXlite.MapTheme == ODNBXlite.THEME_WOODS)){
-            ODNBXlite.SnowCovered = (new Random()).nextInt(ODNBXlite.MapTheme == ODNBXlite.THEME_WOODS ? 2 : 4) == 0;
+        if (canSnow()){
+            if (snow == 1){
+                ODNBXlite.SnowCovered = (new Random()).nextInt(ODNBXlite.MapTheme == ODNBXlite.THEME_WOODS ? 2 : 4) == 0;
+            }else{
+                ODNBXlite.SnowCovered = snow > 0;
+            }
+        }else{
+            ODNBXlite.SnowCovered = false;
         }
         ODNBXlite.GenerateNewOres = newores;
     }
@@ -114,6 +134,7 @@ public class PageAlpha extends Page{
     public void loadFromWorldInfo(WorldInfo w){
         theme = w.mapTheme;
         newores = w.newOres;
+        snow = w.snowCovered ? 2 : 0;
         ODNBXlite.SnowCovered = w.snowCovered;
         ODNBXlite.MapTheme = theme;
         ODNBXlite.GenerateNewOres = newores;
@@ -131,5 +152,9 @@ public class PageAlpha extends Page{
             str.append(")");
         }
         return str.toString();
+    }
+
+    private boolean canSnow(){
+        return features == 3 && (theme == ODNBXlite.THEME_NORMAL || theme == ODNBXlite.THEME_WOODS);
     }
 }
