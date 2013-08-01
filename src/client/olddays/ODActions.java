8426a93779508db65eb4e78937d833df92b6e8fa
Exalm <exalm7659@gmail.com>
2013-08-02 02:17:43 +0600

    Oops

--- src/client/olddays/ODActions.java
+++ src/client/olddays/ODActions.java
@@ -69,8 +69,7 @@ public class ODActions extends OldDaysModule{
     @Override
     public void catchKeyEvent(KeyBinding keybinding){
         if (keybinding==keyFog && minecraft.currentScreen==null && FogKey){
-            boolean flag = org.lwjgl.input.Keyboard.isKeyDown(42) || org.lwjgl.input.Keyboard.isKeyDown(54);
-            minecraft.gameSettings.setOptionValue(EnumOptions.RENDER_DISTANCE, flag ? -1 : 1);
+            minecraft.gameSettings.setOptionValue(EnumOptions.RENDER_DISTANCE, GuiScreen.isShiftKeyDown() ? -1 : 1);
         }
     }
 
