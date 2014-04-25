6ec1d3360b690a50a2e6d1162ced729c1a40c2fb
gus33000 <gus33000@live.fr>
2014-04-25 09:54:04 +0200

    Cleaned Up the script and fixed typos
    and also removed some debug code

--- build/make.sh
+++ build/make.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd ../..
 python src-mods/build/runtime/make.py "$@"
-read -p "Press enter to continue..."
--- build/runtime/make.py
+++ build/runtime/make.py
@@ -27,17 +27,7 @@ def find(classname):
 	if found is "null":
 		print("Not Found " + classname)
 	return found
-	
-def zip(src, dst):
-    zf = zipfile.ZipFile("%s.zip" % (dst), "w")
-    abs_src = os.path.abspath(src)
-    for dirname, subdirs, files in os.walk(src):
-        for filename in files:
-            absname = os.path.abspath(os.path.join(dirname, filename))
-            arcname = absname[len(abs_src) + 1:]
-            zf.write(absname, arcname)
-    zf.close()
-	
+
 def copy(src, dst):
 	if os.path.isdir(src) == True:
 		shutil.copytree(src, dst)
@@ -58,18 +48,17 @@ if os.name is "nt":
 	os.system("runtime\\bin\\python\\python_mcp.exe runtime\\recompile.py")
 	os.system("runtime\\bin\\python\\python_mcp.exe runtime\\reobfuscate.py")
 else:
-	os.system("python runtime\\recompile.py"
-	os.system("python runtime\\reobfuscate.py"
+	os.system("python runtime\\recompile.py")
+	os.system("python runtime\\reobfuscate.py")
 
-UNUSED_LIST = [line.strip() for line in open( 'src-mods/build/conf_make/make_unused.txt' )]
-for Unfile in UNUSED_LIST:
+for Unfile in [line.strip() for line in open('src-mods/build/conf_make/make_unused.txt')]:
 	if os.path.isdir(reobf + Unfile) == True:
 		move(reobf + Unfile, RESULT_DIR + "/client/unused/" + Unfile)
 	else:
 		move(reobf + Unfile + ".class", RESULT_DIR + "/client/unused/")
 
-MODS_LIST = [line.strip() for line in open( 'src-mods/build/conf_make/make_mods_list.txt' )]
-for mod in MODS_LIST :
+
+for mod in [line.strip() for line in open('src-mods/build/conf_make/make_mods_list.txt')]:
 	if os.path.exists('src-mods/build/conf_make/make_mv_' + mod + '.txt'):
 		mv = [line.strip() for line in open('src-mods/build/conf_make/make_mv_' + mod + '.txt')]
 		for mvfilename in mv :
@@ -84,7 +73,6 @@ for mod in MODS_LIST :
 					file = find(mvfilename)
 					if file is not "null":
 						move(file, RESULT_DIR + "/client/" + mod + "/")
-
 	if os.path.exists('src-mods/build/conf_make/make_cp_' + mod + '.txt'):
 		cp = [line.strip() for line in open('src-mods/build/conf_make/make_cp_' + mod + '.txt')]
 		for cpfilename in cp :
@@ -94,7 +82,6 @@ for mod in MODS_LIST :
 				file = find(cpfilename)
 				if file is not "null":
 					copy(file, RESULT_DIR + "/client/" + mod + "/")
-				
 	if os.path.exists('src-mods/build/conf_make/make_add_' + mod + '.txt'):
 		add = [line.strip() for line in open('src-mods/build/conf_make/make_add_' + mod + '.txt')]
 		for addfilename in add :
@@ -105,21 +92,29 @@ copy(currentpath + "/lib/worldedit.jar", RESULT_DIR + "/client/spc/worldedit.jar
 
 if os.path.isdir(RESULT_DIR2 + "/Client/") == False:
 	os.makedirs(os.path.dirname(RESULT_DIR2 + "/Client/"))
-for mod in MODS_LIST :
+
+for mod in [line.strip() for line in open('src-mods/build/conf_make/make_mods_list.txt')]:
 	print("Packaging " + mod + ".zip ...")
-	zip(RESULT_DIR + "/Client/" + mod, RESULT_DIR2 + "/Client/" + mod)
+	zf = zipfile.ZipFile("%s.zip" % (RESULT_DIR2 + "/Client/" + mod), "w")
+	abs_src = os.path.abspath(RESULT_DIR + "/Client/" + mod)
+	for dirname, subdirs, files in os.walk(RESULT_DIR + "/Client/" + mod):
+		for filename in files:
+			absname = os.path.abspath(os.path.join(dirname, filename))
+			arcname = absname[len(abs_src) + 1:]
+			zf.write(absname, arcname)
+	zf.close()
 	
 print("Packaging source code ...")
 src_zip = zipfile.ZipFile(RESULT_DIR2 + "/Client/src.zip", "w" )
 src_zip.write("src-mods/Readme.md", "Readme.md", zipfile.ZIP_DEFLATED )
-
 srcdir = ["src-mods/src/client/nbxlite/", "src-mods/src/client/olddays/", "src-mods/src/client/spawnhuman/", "src-mods/src/client/ssp/", "src-mods/src/client-client/", "src-mods/src/client-server/", "src-mods/src/server/", "src-mods/resources/", "src-mods/build/"]
 for dir in srcdir:
 	for dir_, _, files in os.walk(dir):
 		for fileName in files:
-			src_zip.write(dir_ + "/" + fileName, dir_.replace("src-mods/", "") + fileName, zipfile.ZIP_DEFLATED )
+			src_zip.write(dir_ + "/" + fileName, dir_.replace("src-mods/", "") + "/" + fileName, zipfile.ZIP_DEFLATED)
 
+print("Packaging spc source code ...")
 spc_src_zip = zipfile.ZipFile(RESULT_DIR2 + "/Client/spc-src.zip", "w" )
 for dir_, _, files in os.walk("src-mods/src/client/spc/"):
 	for fileName in files:
-		spc_src_zip.write(dir_ + "/" + fileName, dir_.replace("src-mods/", "") + fileName, zipfile.ZIP_DEFLATED )
+		spc_src_zip.write(dir_ + "/" + fileName, dir_.replace("src-mods/", "") + "/" + fileName, zipfile.ZIP_DEFLATED)
